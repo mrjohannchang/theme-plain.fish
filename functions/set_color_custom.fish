83f8f8358db72e803e06534fdffa839fa7a77708
Henry Chang <jrhenry@flotechnologies.com>
2017-02-07 18:56:14 -0800

    Roll back to old fish color scheme
    Because the new default color scheme doesn't work well in light
    background.

---
+++ functions/set_color_custom.fish
@@ -0,0 +1,26 @@
+function set_color_custom
+    set -U fish_color_normal normal
+    set -U fish_color_command 005fd7 purple
+    set -U fish_color_param 00afff cyan
+    set -U fish_color_redirection 005fd7
+    set -U fish_color_comment 600
+    set -U fish_color_error red --bold
+    set -U fish_color_escape cyan
+    set -U fish_color_operator cyan
+    set -U fish_color_end green
+    set -U fish_color_quote brown
+    set -U fish_color_autosuggestion 555 yellow
+    set -U fish_color_user green
+    set -U fish_color_valid_path --underline
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_match cyan
+    set -U fish_color_search_match --background=purple
+    set -U fish_color_selection --background=purple
+    set -U fish_pager_color_prefix cyan
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description 555 yellow
+    set -U fish_pager_color_progress cyan
+    set -U fish_color_history_current cyan
+    set -U fish_color_host normal
+end
